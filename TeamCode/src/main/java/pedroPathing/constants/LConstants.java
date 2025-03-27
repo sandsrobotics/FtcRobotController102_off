2f49a85ed6cc3c49629d21aff5e1477c78835b54
Tom Kroon <soccer@kroon.ws>
2025-03-27 18:58:49 -0400

    added pedroPathing un tested or tuned

---
+++ TeamCode/src/main/java/pedroPathing/constants/LConstants.java
@@ -0,0 +1,25 @@
+package pedroPathing.constants;
+
+import com.pedropathing.localization.*;
+import com.pedropathing.localization.constants.*;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+public class LConstants {
+    static {
+        PinpointConstants.forwardY = 1; //44.0;
+        PinpointConstants.strafeX = -2.5; // -80.0;
+        PinpointConstants.distanceUnit = DistanceUnit.INCH;
+        PinpointConstants.hardwareMapName = "pinpoint";
+        PinpointConstants.useYawScalar = false;
+        PinpointConstants.yawScalar = 1.0;
+        PinpointConstants.useCustomEncoderResolution = false;
+        PinpointConstants.encoderResolution = GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD;
+        PinpointConstants.customEncoderResolution = 13.26291192;
+        PinpointConstants.forwardEncoderDirection = GoBildaPinpointDriver.EncoderDirection.FORWARD;
+        PinpointConstants.strafeEncoderDirection = GoBildaPinpointDriver.EncoderDirection.FORWARD;
+    }
+}
+
+
+
+
